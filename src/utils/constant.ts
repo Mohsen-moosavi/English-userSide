641ac34602f27478c3c18a823232d3c8fafbc056
Mohsen-moosavi <mohsen.moosavi.saee@gmail.com>
2025-08-15 11:53:25 -0700

    change base_api-url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 const constant = {
-    BASE_API_URL : 'https://english-backend.liara.run/api/v1',
+    // BASE_API_URL : 'https://english-backend.liara.run/api/v1',
     // BACKEND_URL : 'english-backend.liara.run'
-    // BASE_API_URL : 'http://localhost:4000/api/v1'
+    BASE_API_URL : 'http://localhost:4000/api/v1'
 }
 
 // {
